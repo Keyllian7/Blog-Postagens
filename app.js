9aff360fc6af44a22d7c5981f684df9ada2cb5d0
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-06 18:50:32 -0300

    Rotas em arquivos separados e de forma organizada

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const app = express()
+const admin = require('./routes/admin')
 // const mongoose = require('mongoose')
 
 // Configurações
@@ -23,7 +24,7 @@ const app = express()
     // Outras configurações
 
 // Rotas
-
+    app.use('/admin', admin)
 // Outros
 const port = 8081
 app.listen(port, () => {
