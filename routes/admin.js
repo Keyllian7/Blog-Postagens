76a75a3d48c68e697aa959372a5c94fdc27eb597
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-12 14:53:22 -0300

    Listando as postagens

--- routes/admin.js
+++ routes/admin.js
@@ -104,7 +104,12 @@ router.post('/categorias/deletar', (req, res) => {
 })
 
 router.get('/postagens', (req, res) => {
-    res.render("admin/postagens")
+   Postagem.find().populate("categoria").sort({data: "desc"}).then((postagens) => {
+    res.render("admin/postagens", {postagens: postagens})
+   }).catch((err) => {
+    req.flash("mensagem_erro", "Erro ao listar postagens")
+    res.redirect("/admin")
+   })
 })
 
 router.get('/postagens/adicionar', (req, res) => {
