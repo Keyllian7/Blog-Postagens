6d325d1103b301ca33d4207393c5e696a40b63d0
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-12-12 13:25:19 -0300

    Salvando postagens no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 require('../models/Categoria')
 const Categoria = mongoose.model('categorias')
+require('../models/Postagem')
+const Postagem = mongoose.model('postagens')
 
 router.get('/', (req, res) => {
     res.render("admin/index")
@@ -108,6 +110,34 @@ router.get('/postagens/adicionar', (req, res) => {
         req.flash("mensagem_erro", "Erro ao carregar formulário")
         res.redirect("/admin")
     })
+})
+
+router.post('/postagens/nova', (req, res) => {
+    var erros = []
+
+    if(req.body.categoria == "0"){
+        erros.push({texto: "Categoria inválida, registre uma categoria"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/adicionar-postagens", {erros: erros})
+    } else {
+        const novaPostagem = {
+            titulo: req.body.titulo,
+            descricao: req.body.descricao,
+            conteudo: req.body.conteudo,
+            categoria: req.body.categoria,
+            slug: req.body.slug
+        }
+
+        new Postagem(novaPostagem).save().then(() => {
+            req.flash("mensagem_sucesso", "Postagem criada com sucesso")
+            res.redirect("/admin/postagens")
+        }).catch((err) => {
+            req.flash("mensagem_erro", 'Erro ao criar postagem')
+            res.redirect("/admin/postagens")
+        })
+    }
 })
 
 module.exports = router
